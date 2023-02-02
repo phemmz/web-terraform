747b83b7e907f9562c8c491e3cc927f5ffeb294c
Adetunji Femi <phemmzmcllroy@gmail.com>
2023-02-02 23:02:36 +0100

    Update main.tf

--- 03_02-03_remotestate/main.tf
+++ 03_02-03_remotestate/main.tf
@@ -38,19 +38,19 @@ provider "aws" {
 # //////////////////////////////
 # MODULES
 # //////////////////////////////
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+# module "vpc" {
+#   source = "terraform-aws-modules/vpc/aws"
 
-  name = "vpc-module-example"
+#   name = "vpc-module-example"
 
-  cidr = "10.0.0.0/16"
+#   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+#   azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
+#   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+#   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+#   enable_nat_gateway = true
+#   single_nat_gateway = true
   
-  experiments = [variable_validation]
-}
+#   experiments = [variable_validation]
+# }
